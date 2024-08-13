a9fa8adc355e7f12001af368060ba80f74d90487
Satyam Pandey <satyampandey2266@gmail.com>
2024-08-13 12:47:13 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -38,6 +39,8 @@ app.get('/menu', async (req, res) => {
 // Use the personRoutes module
 const personRoutes = require('./routes/personRoutes');
 app.use('/person', personRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
